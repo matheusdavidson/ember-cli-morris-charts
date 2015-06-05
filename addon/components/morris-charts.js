cc4f2a0288731ad824d0a3a41f8237b656e247ea
Thibaut Géry <thibaut.gery@gmail.com>
2015-06-05 11:28:00 +0200

    add all the option of morrisJS graphs

--- addon/components/morris-charts.js
+++ addon/components/morris-charts.js
@@ -50,6 +50,118 @@ default Ember.Component.extend({
             options.xLabelFormat = this.get('xLabelFormat');
         }
 
+        if (this.get('lineColors')) {
+            options.lineColors = this.get('lineColors');
+        }
+
+        if (this.get('lineWidth')) {
+            options.lineWidth = this.get('lineWidth');
+        }
+
+        if (this.get('pointSize')) {
+            options.pointSize = this.get('pointSize');
+        }
+
+        if (this.get('pointFillColors')) {
+            options.pointFillColors = this.get('pointFillColors');
+        }
+
+        if (this.get('pointStrokeColors')) {
+            options.pointStrokeColors = this.get('pointStrokeColors');
+        }
+
+        if (this.get('ymax')) {
+            options.ymax = this.get('ymax');
+        }
+
+        if (this.get('ymin')) {
+            options.ymin = this.get('ymin');
+        }
+
+        if (this.get('smooth')) {
+            options.smooth = this.get('smooth');
+        }
+
+        if (this.get('hideHover')) {
+            options.hideHover = this.get('hideHover');
+        }
+
+        if (this.get('parseTime')) {
+            options.parseTime = this.get('parseTime');
+        }
+
+        if (this.get('units')) {
+            options.units = this.get('units');
+        }
+
+        if (this.get('postUnits')) {
+            options.postUnits = this.get('postUnits');
+        }
+
+        if (this.get('preUnits')) {
+            options.preUnits = this.get('preUnits');
+        }
+
+        if (this.get('dateFormat')) {
+            options.dateFormat = this.get('dateFormat');
+        }
+
+        if (this.get('xLabels')) {
+            options.xLabels = this.get('xLabels');
+        }
+
+        if (this.get('goals')) {
+            options.goals = this.get('goals');
+        }
+
+        if (this.get('goalStrokeWidth')) {
+            options.goalStrokeWidth = this.get('goalStrokeWidth');
+        }
+
+        if (this.get('goalLineColors')) {
+            options.goalLineColors = this.get('goalLineColors');
+        }
+
+        if (this.get('events')) {
+            options.events = this.get('events');
+        }
+
+        if (this.get('eventStrokeWidth')) {
+            options.eventStrokeWidth = this.get('eventStrokeWidth');
+        }
+
+        if (this.get('eventLineColors')) {
+            options.eventLineColors = this.get('eventLineColors');
+        }
+
+        if (this.get('axes')) {
+            options.axes = this.get('axes');
+        }
+
+        if (this.get('grid')) {
+            options.grid = this.get('grid');
+        }
+
+        if (this.get('gridTextColor')) {
+            options.gridTextColor = this.get('gridTextColor');
+        }
+
+        if (this.get('gridTextSize')) {
+            options.gridTextSize = this.get('gridTextSize');
+        }
+
+        if (this.get('gridTextFamily')) {
+            options.gridTextFamily = this.get('gridTextFamily');
+        }
+
+        if (this.get('gridTextWeight')) {
+            options.gridTextWeight = this.get('gridTextWeight');
+        }
+
+        if (this.get('fillOpacity')) {
+            options.fillOpacity = this.get('fillOpacity');
+        }
+
         if (this.get('parseTime') === 0) {
             options.parseTime = false;
         }
